9656bffba040e09bb87a88da0156262e01b54995
Julio <105402363+Ju-silveira@users.noreply.github.com>
2024-08-07 21:23:42 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const elementos = document.querySelectorAll('.movimento')
+
+const obisevar = new IntersectionObserver((entrada) => {
+    entrada.forEach( (visor) => {
+        if(visor.isIntersecting){
+            visor.target.classList.add('visivel')
+        } else{
+            visor.target.classList.remove('visivel')
+        }
+    })
+})
+
+elementos.forEach( (elemento) => obisevar.observe(elemento))
+
+
+const elementosRV = document.querySelectorAll('.movimentoRV')
+
+const obisevarRV = new IntersectionObserver((entradaRV) => {
+    entrada.forEach( (visorRV) => {
+        if(visorRV.isIntersecting){
+            visorRV.target.classList.add('visivelRV')
+        } else{
+            visorRV.target.classList.remove('visivelRV')
+        }
+    })
+})
+
+elementosRV.forEach( (elemento) => obisevarRV.observe(elemento))
